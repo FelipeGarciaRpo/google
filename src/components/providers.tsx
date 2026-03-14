cdecad742133afebf8c5e40ef625f95589230414
felipe <felipe.garciar@upb.edu.co>
2026-03-13 23:50:11 -0500

    08: ide layout

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -20,7 +20,6 @@ export const Providers = ({children}: {children: React.ReactNode})=>{
                     disableTransitionOnChange
                 >
                     <Authenticated>
-                        <UserButton/>
                         {children}
                     </Authenticated>
                     <Unauthenticated>
